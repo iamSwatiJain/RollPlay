fbf312bdbaa74053c98eb4f49896cd5d21f17632
Swati Jain <swati@jain.se>
2018-01-04 13:47:42 +0100

    Add project files.

---
+++ RollPlay/Numbers.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RollPlay
+{
+    class Numbers
+    {
+        Random rand = new Random();
+
+        public int[] ThrowDice(int numberOfThrows)
+        {
+            int[] randomNumberArray = new int[numberOfThrows];
+            for (int i = 0; i < numberOfThrows; i++)
+            {
+                randomNumberArray[i] = rand.Next(1, 6);
+            }
+            return randomNumberArray;
+        }
+
+        public int[] ThrowDice(int diceSides, int numberOfThrows)
+        {
+            int[] randomNumberArray = new int[numberOfThrows];
+            for (int i=0; i < numberOfThrows; i++ )
+            {
+                randomNumberArray[i] = rand.Next(1, diceSides);
+            }
+            return randomNumberArray;
+        }
+
+        public int[] ThrowDice(string diceAndThrows)
+        {
+            String[] substring = diceAndThrows.Split('T');
+            int numberOfThrows = Int32.Parse(substring[0]);
+            int diceSides = Int32.Parse(substring[1]);
+
+            int[] randomNumberArray = new int[numberOfThrows];
+            for (int i = 0; i < numberOfThrows; i++)
+            {
+                randomNumberArray[i] = rand.Next(1, diceSides);
+            }
+            return randomNumberArray;
+        }
+    }
+}
---
+++ RollPlay/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RollPlay
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Numbers number = new Numbers();
+
+            // Console.WriteLine("Enter the number of sides your dice has:");
+            // int diceSides = Int32.Parse(Console.ReadLine()); //Convert.ToInt32 handle null and returns '0' as output and int.parse is not going to handle NULL and will give a Argument Null Exception
+
+            // Console.WriteLine("Enter the number of times you want to throw your dice:");
+            // int numberOfThrows = Convert.ToInt32(Console.ReadLine());
+
+            //// int[] randomNumbers = number.ThrowDice(numberOfThrows);
+            // int[] randomNumbers = number.ThrowDice(diceSides, numberOfThrows);
+            int[] randomNumbers = number.ThrowDice("6T20");
+
+
+            Console.WriteLine("The results of throw are:");
+            foreach (int rand in randomNumbers)
+            {
+                Console.WriteLine(rand);
+            }
+        }
+    }
+}
